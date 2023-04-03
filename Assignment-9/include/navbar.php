d0953a256868d70286cd9b78fc8df77cfcf1d5e8
shahidulgpi <106987384+shahidulgpi@users.noreply.github.com>
2023-04-03 14:17:19 +0600

    Add files via upload

---
+++ Assignment-9/include/navbar.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg">
+        <div class="container">
+          <a class="navbar-brand" href="index.php"><h2>Stand Blog<em>.</em></h2></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>               
+              <li class="nav-item">
+                <a class="nav-link" href="blog.php">Blog</a>
+              </li>             
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
